69334ca8affcf654a0b8f15b05d0ae4145b178fe
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-07 20:30:02 +0700

    Create BlockPlaceEvent.php

---
+++ src/NurAzliYT/LandProtections/events/BlockPlaceEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NurAzliYT\LandProtections\events;
+
+use pocketmine\event\Cancellable;
+use pocketmine\event\Event;
+use pocketmine\player\Player;
+use pocketmine\block\Block;
+use pocketmine\world\Position;
+
+class BlockPlaceEvent extends Event implements Cancellable {
+    private Player $player;
+    private Block $block;
+    private Position $position;
+
+    public function __construct(Player $player, Block $block, Position $position) {
+        $this->player = $player;
+        $this->block = $block;
+        $this->position = $position;
+    }
+
+    public function getPlayer(): Player {
+        return $this->player;
+    }
+
+    public function getBlock(): Block {
+        return $this->block;
+    }
+
+    public function getPosition(): Position {
+        return $this->position;
+    }
+
+    public function cancel(): void {
+        $this->setCancelled();
+    }
+}
