6ed50aeac2d639ff1e8521d515cba71244c8d482
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-07 21:05:39 +0700

    Create Test.php

---
+++ tests/Unit/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NurAzliYT\LandProtections\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
